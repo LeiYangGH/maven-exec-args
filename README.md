989ad1d630c56bf9c2480d6e879c8f89bf37a456
LeiYangGH <675686066@qq.com>
2021-08-04 09:55:45 +0800

    argparse4j app

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # maven-exec-args
 experiments with exec-maven-plugin, exec:exec and exec:java, with arguments.
+
+# app
+```bash
+com.Main
+-n ly -g F
+```
--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,12 +1,35 @@
 package com;
 
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import net.sourceforge.argparse4j.inf.Namespace;
+
 public class Main {
 
   public static void main(String[] args) {
     System.out.println("Main.main.start");
+    ArgumentParser parser = ArgumentParsers.newFor("exec-experiments").build()
+        .defaultHelp(true)
+        .description("arg parse.");
+    parser.addArgument("-n", "--name")
+        .setDefault("unknown")
+        .help("Your name");
+    parser.addArgument("-g", "--gender")
+        .choices("F", "M").setDefault("M")
+        .help("Your gender (F,M)");
     for (String arg : args) {
       System.out.println(arg);
     }
+    Namespace ns = null;
+    try {
+      ns = parser.parseArgs(args);
+    } catch (ArgumentParserException e) {
+      parser.handleError(e);
+      System.exit(1);
+    }
+    System.out.println(String.format("name=%s,gender=%s", ns.getString("name"),
+        ns.getString("gender")));
     System.out.println("Main.main.end");
   }
 }
